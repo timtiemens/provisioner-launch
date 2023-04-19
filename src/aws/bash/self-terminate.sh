df23542e639927fd8f3e0d363ec06c422c472573
Tim Tiemens <timtiemens@fakeusers.noreply.github.com>
2023-04-19 10:57:02 -0500

    instance_id using token

--- src/aws/bash/self-terminate.sh
+++ src/aws/bash/self-terminate.sh
@@ -16,5 +16,5 @@
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 INSTANCE_ID=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id` 
 
-aws ec2 terminate-instances --instance-ids $(INSTANCE_ID)
+aws ec2 terminate-instances --instance-ids $INSTANCE_ID
 
