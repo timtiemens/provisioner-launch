b93837c17ede372158d059461b2e3216667f7278
Tim Tiemens <timtiemens@fakeusers.noreply.github.com>
2023-04-19 10:55:08 -0500

    update to use token to talk to metadata server

--- src/aws/bash/self-terminate.sh
+++ src/aws/bash/self-terminate.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 #
-# IMPORTANT - this script requires permissions
-#
-# you can test your permissions with
-#   $ aws ec2 describe-instances
+# CAUTION -- this script will completely destroy the AWS EC2 instance
+#         --  that it is running in (aka "terminate")
 #
 
-aws ec2 terminate-instances --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+#
+# IMPORTANT - this script requires permissions
+#           - you can test your permissions with this command
+#                 $ aws ec2 describe-instances
+#
+
+# NOTE: IMDSv2 requires a TOKEN to talk to metadata endpoint
+
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+INSTANCE_ID=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id` 
+
+aws ec2 terminate-instances --instance-ids $(INSTANCE_ID)
+
