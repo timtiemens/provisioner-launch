0daa2690306124fffa27418ab9280800f216b7c8
Tim Tiemens <timtiemens@fakeusers.noreply.github.com>
2023-04-23 02:31:52 -0500

    update comments

--- src/projects/ml-nst/runml-nst.sh
+++ src/projects/ml-nst/runml-nst.sh
@@ -23,5 +23,5 @@ bash $PROJECT_DIR/src/helpers/project-chown.sh
 # run the project-specific code in the "-normal" script
 sudo -u $(src/helpers/get-normal-user.sh) --login  bash $PROJECT_DIR/src/projects/ml-nst/runml-nst-normal.sh $PROJECT_DIR 
 
-# note: this project (ml-nst) finishes with a "self-terminate.sh"
+# note: that script (runml-nst-normal) finishes with a "self-terminate.sh"
 
